549e19e38678647ee72d8074520c3298cb301551
ogabek <quvvatullayev@gmail.com>
2023-09-23 14:09:30 +0500

    fix category model

--- product/models.py
+++ product/models.py
@@ -3,3 +3,8 @@ from django.db import models
 class Category(models.Model):
     category_name = models.CharField(max_length=120)
     
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self) -> str:
+        return self.category_name
