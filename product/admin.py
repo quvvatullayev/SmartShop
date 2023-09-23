b8bfbdc16e817e19c13f21182256c30b0b88d527
ogabek <quvvatullayev@gmail.com>
2023-09-23 14:29:39 +0500

    product models are add in admin ponel

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from .models import Category, Product
 
 # Register your models here.
+admin.site.unregister([Group])
+admin.site.register([Category, Product])
