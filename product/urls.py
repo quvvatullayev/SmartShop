df44424995324efb5f71d21d65e34a975f2424c8
ogabek <quvvatullayev@gmail.com>
2023-09-23 17:19:30 +0500

    make things

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views.category import ListCreateView
+from .views.product import ProductListCreateView
 
 app_name = 'product'
 urlpatterns = [
-    path('', ListCreateView.as_view(), name = 'categorys')
+    path('', ListCreateView.as_view(), name = 'categorys'),
+    path('category/product/<int:pk>/', ProductListCreateView.as_view(), name='category_by_product')
 ]
---
+++ product/views/product.py
@@ -0,0 +1,26 @@
+from rest_framework import generics
+from rest_framework.response import Response
+from ..serializers import Category, CategorySerializer
+from ..serializers import Product, ProductSerializer
+from django.shortcuts import render
+
+class ProductRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def list(self, request, pk:int):
+        product_objs = Product.objects.filter(category = pk)
+        product = ProductSerializer(product_objs, many = True)
+
+        category_objs = Category.objects.all()
+        categorys = CategorySerializer(category_objs, many = True)
+
+        return render(
+            request=request, 
+            template_name='category.html', 
+            context={'product':product.data}
+            )
