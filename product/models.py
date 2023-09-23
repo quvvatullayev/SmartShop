d0938f2b6ec61838d7cda324247d6cfbcf9f7f91
ogabek <quvvatullayev@gmail.com>
2023-09-23 14:24:09 +0500

    create product model

--- product/models.py
+++ product/models.py
@@ -1,10 +1,25 @@
 from django.db import models
 
 class Category(models.Model):
-    category_name = models.CharField(max_length=120)
+    title = models.CharField(max_length=120)
     
     class Meta:
         ordering = ['-id']
 
     def __str__(self) -> str:
-        return self.category_name
+        return self.title
+    
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    img = models.ImageField(upload_to='../product_imgs')
+    quantity = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self) -> str:
+        return self.title
+    
