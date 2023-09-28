599d6aea315a3265c818b5ef61637972e77b7b45
ogabek <quvvatullayev@gmail.com>
2023-09-28 13:29:09 +0500

    fix

--- core/urls.py
+++ core/urls.py
@@ -3,9 +3,10 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
+url_name = 'base'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('product.urls')),
-    path('user/', include('user.urls')),
+    path('user/', include('user.urls'), name='user'),
     path('auth/', include('rest_framework.urls'), name = 'aouth')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
