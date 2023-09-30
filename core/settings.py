2ec861c02d134332ef725eeddc7de5fbc4f7f20f
ogabek <quvvatullayev@gmail.com>
2023-09-30 16:11:35 +0500

    create user registration

--- core/settings.py
+++ core/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+SITE_ID = 1
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,8 +39,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     "rest_framework",
     'rest_framework.authtoken',
+    "dj_rest_auth",
+    "dj_rest_auth.registration",
+    "allauth",
+    'allauth.account',
+    "corsheaders",
+    # 'allauth.socialaccount',
 
     # local apps
     'product',
@@ -49,6 +57,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -57,7 +67,11 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
+        'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        "rest_framework.authentication.SessionAuthentication",
         'rest_framework.authentication.TokenAuthentication',  # <-- And here
     ],
 }
@@ -75,10 +89,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                "django.template.context_processors.request",
             ],
         },
     },
 ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
--- core/urls.py
+++ core/urls.py
@@ -2,14 +2,11 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework.authtoken.views import obtain_auth_token
-from django.contrib.auth import views as auth_views
 
 url_name = 'base'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('product.urls')),
-    path('user/', include('user.urls'), name='user'),
-    path('login/', auth_views.LoginView.as_view(), name='my_custom_login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='my_custom_logout'),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
